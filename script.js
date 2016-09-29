490aca8b1ebd689e5635ba339c2ae528e2571eb7
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-09-30 01:47:00 +0530

    Build a document editor

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function toggleEditor () {
+    
+    var e = document.getElementById('editorPad');
+    
+    if (e.style.display == 'none' || e.style.display === '')
+    {
+        e.style.display = 'flex';
+        var pageCode =  document.getElementById('pageContainer').innerHTML;
+        document.getElementById('codeInput').value = pageCode;
+        
+    }
+    else 
+    {
+        e.style.display = 'none';
+    }
+    
+}
+
+
+function updatePage () {
+   var codeInput =  document.getElementById('codeInput').value;
+    document.getElementById('pageContainer').innerHTML = codeInput;
+}
+
+
 function printContent(el){
 	var restorepage = document.body.innerHTML;
 	var printcontent = document.getElementById(el).innerHTML;
