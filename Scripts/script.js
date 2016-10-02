4b9b7bb22d619a4a409ed9277fadb3d0520158b1
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-10-02 13:34:12 +0530

    add styles to editor, viewer and toolBar restructure script.
    RELEASE USABLE VERSION

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,32 +1,29 @@
-function toggleEditor () {
+function toggleEditor() {
     
     var e = document.getElementById('editorPad');
     
-    if (e.style.display == 'none' || e.style.display === '')
-    {
+    if (e.style.display === 'none' || e.style.display === '') {
         e.style.display = 'flex';
-        var pageCode =  document.getElementById('pageContainer').innerHTML;
-        document.getElementById('codeInput').value = pageCode;
+        document.getElementById('codeInput').value = document.getElementById('pageContainer').innerHTML;
         
-    }
-    else 
-    {
+    } else {
         e.style.display = 'none';
     }
     
 }
 
 
-function updatePage () {
-   var codeInput =  document.getElementById('codeInput').value;
+function updatePage() {
+    var codeInput =  document.getElementById('codeInput').value;
     document.getElementById('pageContainer').innerHTML = codeInput;
 }
 
 
-function printContent(el){
+function printContent(el) {
 	var restorepage = document.body.innerHTML;
 	var printcontent = document.getElementById(el).innerHTML;
 	document.body.innerHTML = printcontent;
 	window.print();
 	document.body.innerHTML = restorepage;
+    document.getElementById('editorPad').style.display = "none";
 }
