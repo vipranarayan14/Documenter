f22d36233236866d058f0eb41fa5d1cab244ba64
Prasanna Venkatesh <vipranarayan14@gmail.com>
2016-10-08 16:46:36 +0530

    open another window to print

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,16 +1,17 @@
 function toggleEditor() {
-    
+
     var e = document.getElementById('editorPad');
-    
+
     if (e.style.display === 'none' || e.style.display === '') {
         e.style.display = 'flex';
         //If input is a textarea instead of Ace Editor
         //document.getElementById('codeInput').value = document.getElementById('pageContainer').innerHTML;
-        
-    } else {
+
+    }
+    else {
         e.style.display = 'none';
     }
-    
+
 }
 
 //If input is a textarea instead of Ace Editor
@@ -21,10 +22,14 @@ function toggleEditor() {
 
 
 function printContent(el) {
-	var restorepage = document.body.innerHTML;
-	var printcontent = document.getElementById(el).innerHTML;
-	document.body.innerHTML = printcontent;
-	window.print();
-	document.body.innerHTML = restorepage;
+
+    //var win = window
+    //.open("", "Title", "toolbar=no, location=no, directories=no, status=no, menubar=no");
+    //win.document.body.innerHTML = document.getElementById(el).innerHTML;
+    var restorepage = document.body.innerHTML;
+    var printcontent = document.getElementById(el).innerHTML;
+    document.body.innerHTML = printcontent;
+    window.print();
+    document.body.innerHTML = restorepage;
     document.getElementById('editorPad').style.display = "none";
 }
